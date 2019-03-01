694a04749bbdcdab2788c7b83dc484094e48b145
RSL <sconds@gmail.com>
2019-03-01 15:53:39 -0500

    initial scaffold-y commit!

---
+++ lib/rtist.rb
@@ -0,0 +1,12 @@
+# Dependencies first
+require 'rubygems'
+require 'rmagick'
+
+# Now Rtist code
+require 'rtist/version'
+
+module Rtist
+  class Error < StandardError; end
+
+  # Your code goes here...
+end
---
+++ lib/rtist/version.rb
@@ -0,0 +1,3 @@
+module Rtist
+  VERSION = "0.0.1"
+end
